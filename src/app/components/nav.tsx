e6d018ae2703fbab27bd617755b508c9272ab700
JEFFERSON TEIXEIRA <jefferson_teixeira1@estudante.sesisenai.org.br>
2025-05-05 19:02:26 -0300

    Valida login

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -1,6 +1,12 @@
+"use client";
+
 import Link from 'next/link';
+import { useAuth } from "../lib/firebaseauth";
+import { logout } from "../lib/firebaseauth";
 
 export default function Nav() {
+    const { user } = useAuth();
+
     return (
         <nav className='flex flex-row mt-4 gap-5'>
             <Link
@@ -8,21 +14,34 @@ export default function Nav() {
                 className="text-white hover:text-black hover:bg-white hover:scale-110 transition-transform duration-300 ease-in-out px-4 py-2 rounded-md">
                 Inicio
             </Link>
-            <Link
-                href="/register"
-                className="text-white hover:text-black hover:bg-white hover:scale-110 transition-transform duration-300 ease-in-out px-4 py-2 rounded-md">
-                Registrar
-            </Link>
-            <Link
-                href="/login"
-                className="text-white hover:text-black hover:bg-white hover:scale-110 transition-transform duration-300 ease-in-out px-4 py-2 rounded-md">
-                Login
-            </Link>
-            <Link
-                href="/task"
-                className="text-white hover:text-black hover:bg-white hover:scale-110 transition-transform duration-300 ease-in-out px-4 py-2 rounded-md">
-                Tarefas
-            </Link>
+            {!user && (
+                <>
+                    <Link
+                        href="/register"
+                        className="text-white hover:text-black hover:bg-white hover:scale-110 transition-transform duration-300 ease-in-out px-4 py-2 rounded-md">
+                        Registrar
+                    </Link>
+                    <Link
+                        href="/login"
+                        className="text-white hover:text-black hover:bg-white hover:scale-110 transition-transform duration-300 ease-in-out px-4 py-2 rounded-md">
+                        Login
+                    </Link>
+                </>
+            )}
+            {user && (
+                <>
+                    <Link
+                        href="/task"
+                        className="text-white hover:text-black hover:bg-white hover:scale-110 transition-transform duration-300 ease-in-out px-4 py-2 rounded-md">
+                        Tarefas
+                    </Link>
+                    <button
+                        onClick={logout}
+                        className="text-white hover:text-black hover:bg-white hover:scale-110 transition-transform duration-300 ease-in-out px-4 py-2 rounded-md">
+                        Logout
+                    </button>
+                </>
+            )}
         </nav>
     );
 }
